8d37b687db07ae4579db59fa2f2254c48bf843ca
taketototo <s246153@wakayama-u.ac.jp>
2021-08-04 16:39:33 +0900

    StartScreenからColorFilterScreen(P)への画面遷移

--- app/src/main/java/com/example/clearschedule/StartscreenActivity.java
+++ app/src/main/java/com/example/clearschedule/StartscreenActivity.java
@@ -1,6 +1,11 @@
 package com.example.clearschedule;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.ColorFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,13 @@ public class StartscreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(StartscreenActivity.this,ColorfilterActivity.class);
+                startActivity(intent);
+            }
+        }); /*P型のボタンを押してP型のカラーフィルタへの画面遷移*/
     }
 }

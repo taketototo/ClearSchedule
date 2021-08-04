45d0bff18ffbcafcfb6492b5b33aa46a08a0e778
sakanoharuto <s246111@wakayama-u.ac.jp>
2021-08-04 17:12:39 +0900

    カラーフィルターのやつ追加

--- app/src/main/java/com/example/clearschedule/HomescreenActivity.java
+++ app/src/main/java/com/example/clearschedule/HomescreenActivity.java
@@ -1,6 +1,9 @@
 package com.example.clearschedule;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class HomescreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+
+        final Button editbutton = findViewById(R.id.button14);
+
+        editbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HomescreenActivity.this, EditscreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

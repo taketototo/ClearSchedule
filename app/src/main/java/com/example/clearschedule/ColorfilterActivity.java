058ce223727b0a22081d88564d632664ca40f674
taketototo <s246153@wakayama-u.ac.jp>
2021-08-09 18:25:39 +0900

    カラーフィルタ画面からホーム画面への画面遷移

--- app/src/main/java/com/example/clearschedule/ColorfilterActivity.java
+++ app/src/main/java/com/example/clearschedule/ColorfilterActivity.java
@@ -1,6 +1,9 @@
 package com.example.clearschedule;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class ColorfilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colorfilter);
+
+        final Button finishbutton = findViewById(R.id.finishbutton);
+        finishbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ColorfilterActivity.this,HomescreenActivity.class);
+                startActivity(intent);
+            }
+        }); /*カラーフィルタ画面からホーム画面への画面遷移*/
     }
 }
